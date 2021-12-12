979909a125a641f84c190de0183d9c7f2f1fbfa5
alihansarigit <alihansaribas18@gmail.com>
2021-12-13 00:27:52 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(234, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dönüştür";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dosya Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(520, 121);
+            this.listBox1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(403, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Dönüştür (.Webp)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(403, 65);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(59, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Birleştir";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 265);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MangaShip.com - Webp Dönüştürücü";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,153 @@
+﻿using GroupDocs.Conversion;
+using GroupDocs.Conversion.Options.Convert;
+using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string[] DosyaYolu;
+        int sayac = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            sayac = 0;
+            string[] newPath = new string[DosyaYolu.Length-1];
+            if (checkBox1.Checked)
+            {
+                using (ImageFactory factory = new ImageFactory(preserveExifData: false))
+                {
+                    for (int i = 0; i < (DosyaYolu.Length - 1); i++)
+                    {
+                        string path = DosyaYolu[i];
+
+                        FileInfo info = new FileInfo(path);
+                        string directoryPath = info.Directory.FullName + "\\output\\";
+                        string fileName = info.Name.Replace(info.Extension, "");
+
+                        if (!Directory.Exists(directoryPath))
+                        {
+                            Directory.CreateDirectory(directoryPath);
+                        }
+
+                        string output = directoryPath + fileName + ".jpg";
+                        newPath[i] = output;
+
+                        factory.Load(path)
+                        .Format(new JpegFormat())
+                        .Quality(100)
+                        .Save(output);
+                    }
+                }
+            }
+
+            if (checkBox2.Checked)
+            {
+                if(newPath[0] == null)
+                {
+                    newPath = DosyaYolu;
+                }
+
+                List<Bitmap> bitmapList = new List<Bitmap>();
+
+                for (int i = 0; i < newPath.Length; i++)
+                {
+                    bitmapList.Add(new Bitmap(newPath[i]));
+                }
+
+                FileInfo info = new FileInfo(newPath[0]);
+                string dir = info.Directory.FullName+"\\output\\";
+
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                int isim = 0;
+
+                while (bitmapList.Count != 0)
+                {
+                    var outputHeight = 0;
+                    var outputWidth = 0;
+
+                    int sayac = 0;
+                    foreach (var item in bitmapList)
+                    {
+
+                        var sinir = (outputHeight + item.Height);
+                        if (sinir < 30000)
+                        {
+                            sayac++;
+                            outputHeight += item.Height;
+                        }
+                        if (item.Width > outputWidth)
+                        {
+                            outputWidth = item.Width;
+                        }
+                    }
+
+                    Bitmap outputImage = new Bitmap(outputWidth, outputHeight);
+
+                    using (Graphics graphics = Graphics.FromImage(outputImage))
+                    {
+                        int totalHeight = 0;
+                        for (int i = 0; i < sayac; i++)
+                        {
+                            var source = bitmapList[i];
+                            graphics.DrawImage(source,
+                                new Rectangle(0, totalHeight, outputWidth, source.Height),
+                                new Rectangle(0, 0, outputWidth, source.Height), 
+                                GraphicsUnit.Pixel);
+                            totalHeight += source.Height;
+                        }
+                    }
+                    isim++;
+                    outputImage.Save(dir+"output" + isim +".jpg", ImageFormat.Jpeg);
+                    for (int i = 0; i < sayac; i++)
+                    {
+                        bitmapList.RemoveAt(0);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+
+                DosyaYolu = dialog.FileNames;
+
+                foreach (var item in DosyaYolu)
+                {
+                    listBox1.Items.Add(item);
+                }
+                button1.Enabled = true;
+            }
+        }
+    }
+}
